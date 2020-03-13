49c09e8f8ec788d2a61b92bfa02dd0e9b6a38d16
Alicia Buehner <41552555+aliciaNB@users.noreply.github.com>
2020-03-13 14:02:55 -0700

    Initial commit

---
+++ src/test/java/com/herokuapp/it372finalproject/restfulservices/RestfulservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.it372finalproject.restfulservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
