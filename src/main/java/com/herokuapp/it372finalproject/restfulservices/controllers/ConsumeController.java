8831961d3464df5afa95120caf7f351f618c9f87
Alicia Buehner <41552555+aliciaNB@users.noreply.github.com>
2020-03-15 09:57:22 -0700

    basic validation for subscribe form

--- src/main/java/com/herokuapp/it372finalproject/restfulservices/controllers/ConsumeController.java
+++ src/main/java/com/herokuapp/it372finalproject/restfulservices/controllers/ConsumeController.java
@@ -1,21 +1,20 @@
 package com.herokuapp.it372finalproject.restfulservices.controllers;
 
+import javax.validation.Valid;
+
 import com.herokuapp.it372finalproject.restfulservices.model.SubscribeForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
 public class ConsumeController {
-
-//    @RequestMapping("/consume")
-//    public String showHome(Model model)
-//    {
-//        return "consume";
-//    }
 
     @GetMapping("/consume")
     public String subscribeForm(Model model)
@@ -25,9 +24,30 @@ public class ConsumeController {
     }
 
     @PostMapping("/consume")
-    public String subscribeSubmit(@ModelAttribute SubscribeForm subscribe, Model model)
+    public String subscribeSubmit(@Valid @ModelAttribute("subscribe") SubscribeForm subscribe,
+                                  BindingResult bindingResult, Model model)
     {
-        model.addAttribute("subscribe", subscribe);
-        return "result";
+        if (bindingResult.hasErrors())
+        {
+            return "consume";
+        }
+        else
+        {
+            model.addAttribute("subscribe", subscribe);
+            return "result";
+        }
     }
+
+//    @RequestMapping("/consume")
+//    public String showHome(Model model)
+//    {
+//        return "consume";
+//    }
+
+//    @PostMapping("/consume")
+//    public String subscribeSubmit(@ModelAttribute SubscribeForm subscribe, Model model)
+//    {
+//        model.addAttribute("subscribe", subscribe);
+//        return "result";
+//    }
 }
--- src/main/java/com/herokuapp/it372finalproject/restfulservices/model/SubscribeForm.java
+++ src/main/java/com/herokuapp/it372finalproject/restfulservices/model/SubscribeForm.java
@@ -1,9 +1,23 @@
 package com.herokuapp.it372finalproject.restfulservices.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class SubscribeForm {
 
+
+    @Size(min=2, max=40, message="Must be at least 2 - 40 letters")
+    @NotEmpty(message="First Name Required*")
     private String fname;
+
+    @Size(min=2, max=40, message="Must be at least 2 - 40 letters")
+    @NotEmpty(message="Last Name Required*")
     private String lname;
+
+    @Size(min=5, max=254, message="Must be at least 5 - 254 characters")
+    @NotEmpty(message="Email Required*")
+    @Email
     private String email;
 
     public String getFname() {
