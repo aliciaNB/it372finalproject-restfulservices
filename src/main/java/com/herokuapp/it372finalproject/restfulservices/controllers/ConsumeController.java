ace3674c5dc5f3f59be6a20095b5043bc7b3a283
Alicia Buehner <41552555+aliciaNB@users.noreply.github.com>
2020-03-15 07:36:54 -0700

    Add form submission on POST and result page

--- src/main/java/com/herokuapp/it372finalproject/restfulservices/controllers/ConsumeController.java
+++ src/main/java/com/herokuapp/it372finalproject/restfulservices/controllers/ConsumeController.java
@@ -1,15 +1,33 @@
 package com.herokuapp.it372finalproject.restfulservices.controllers;
 
+import com.herokuapp.it372finalproject.restfulservices.model.SubscribeForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ConsumeController {
 
-    @RequestMapping("/consume")
-    public String showHome(Model model)
+//    @RequestMapping("/consume")
+//    public String showHome(Model model)
+//    {
+//        return "consume";
+//    }
+
+    @GetMapping("/consume")
+    public String subscribeForm(Model model)
     {
+        model.addAttribute("subscribe", new SubscribeForm());
         return "consume";
+    }
+
+    @PostMapping("/consume")
+    public String subscribeSubmit(@ModelAttribute SubscribeForm subscribe, Model model)
+    {
+        model.addAttribute("subscribe", subscribe);
+        return "result";
     }
 }
